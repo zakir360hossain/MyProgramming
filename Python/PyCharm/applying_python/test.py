6e3b94425e52e81eac939d19520a4c5ea12e391e
Zakir Hossain <zakir360hossain@gmail.com>
2019-11-04 17:05:07 -0500

    update

--- Python/PyCharm/applying_python/test.py
+++ Python/PyCharm/applying_python/test.py
@@ -1,14 +1,9 @@
-
-
-
-
 def IR(Principle, Rate, Years):
     Real_rate = .01 * Rate
-    total = Principle * (1 + (Real_rate))** Years
-    mm = total-Principle
+    total = Principle * (1 + (Real_rate)) ** Years
+    mm = total - Principle
     print('total', total)
     print("Money made", mm)
 
-print (IR(1000,5,10))
 
-
+print(IR(1000, 5, 10))
