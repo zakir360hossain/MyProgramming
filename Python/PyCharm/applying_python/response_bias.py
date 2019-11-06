ec93d85769d5a6a0dd6ed80a932c0c7d580181e8
Zakir Hossain <zakir360hossain@gmail.com>
2019-11-06 11:04:35 -0500

    updates

--- Python/PyCharm/applying_python/response_bias.py
+++ Python/PyCharm/applying_python/response_bias.py
@@ -1,19 +1,28 @@
 import random
 
-freshmen = [124, 123, 354, 234, 117, 150]
-sophomore = [167, 242, 265, 278, 289, 190]
-junior = [242, 124, 465, 5786, 123, 3, 35]
-senior = [435, 2, 21, 345, 456, 12, 435, 456]
+freshmen = [270, 208, 257, 243, 149, 229, 242, 255, 220, 15, 210, 117, 156]
+sophomore = [254, 247, 232, 146, 239, 233, 13, 222, 108, 215, 244, 238, 206, 212]
+junior = [266, 260, 236, 115, 16, 150, 214, 235, 113, 127, 202, 114]
+senior = [133, 131, 216, 125, 217, 267, 219, 148, 268, 248, 253, 218]
+
+print(freshmen.__len__())
+print(sophomore.__len__())
+print(junior.__len__())
+print(senior.__len__())
 
 random.shuffle(freshmen)
 random.shuffle(sophomore)
 random.shuffle(junior)
 random.shuffle(senior)
-
+print("Iterables shuffled:")
 print(freshmen)
 print(sophomore)
 print(junior)
-print(senior)
+print(senior, "\n")
 
-selected = random.sample(freshmen, 1)
-print(selected)
+print("Sample selected from each iterables:", "\n")
+freshmen_sample = random.sample(freshmen, 1)
+sophomore_sample = random.sample(sophomore, 1)
+junior_sample = random.sample(junior, 1)
+senior_sample = random.sample(senior, 1)
+
