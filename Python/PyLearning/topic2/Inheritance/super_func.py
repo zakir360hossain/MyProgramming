a0372c3f2a7c3339b4acdc26e779a337b9a58f3d
Zakir Hossain <zakir360hossain@gmail.com>
2020-01-23 08:31:38 -0500

    changes

--- Python/PyLearning/topic2/Inheritance/super_func.py
+++ Python/PyLearning/topic2/Inheritance/super_func.py
@@ -1,6 +1,8 @@
-# It allows us to call a method from the parent classes.
+# It allows to call a method from the parent classes.
 
 class Vehicle:
+    """This is the parent class"""
+
     def start(self):
         print("Starting engine")
 
@@ -9,10 +11,16 @@ class Vehicle:
 
 
 class Car(Vehicle):
+    """This is the child class"""
+
     def progress(self):
+        # Method from parent class
         super().start()
         print("I am in motion")
+        # Method from the parent class
         super().stop()
+        # start and stop are method from the parent class Vehicle, and using the super method, those methods are
+        # being called in a method that belongs to the chil class Car.
 
 
 bmw = Car()
